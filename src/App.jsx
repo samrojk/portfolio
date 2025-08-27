7560f75b14703f0f1778b23c3721779e753fca68
Samroj Khan <samrojk7744@gmail.com>
2025-08-27 22:30:27 +0530

    Added the Skills Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar.jsx";
 import Hero from "./components/hero.jsx";
-// import Skills from "./components/skills.jsx";
+import Skills from "./components/skills.jsx";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-primary dark:bg-primary-dark">
       <Navbar />
       <Hero />
-      {/* <Skills /> */}
+      <Skills />
     </div>
   );
 };
